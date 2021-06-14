ec0f995898c7980250bb09c13bc67c2562317d11
dcioara <dan.cioara@nordlogic.com>
2021-06-14 19:54:48 +0300

    adjustments

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = document.getElementsByTagName('base')[0].href.replace(window.location.origin, '') || 'ip';
+export const BASE_PATH = document.getElementsByTagName('base')[0].href.replace(window.location.origin, '').replace('/', '') || 'ip';
 
 export const getDomainByEnv = () => {
   return document.getElementsByTagName('base')[0].href;
