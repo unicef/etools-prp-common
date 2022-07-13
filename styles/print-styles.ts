0614b920cd24ef4033626b289c2d1152cec7c282
Trif Adriana <trif.corina.adriana@gmail.com>
2022-07-13 20:16:05 +0300

    Print styles

--- styles/print-styles.ts
+++ styles/print-styles.ts
@@ -11,6 +11,15 @@ export const printStyles = html`
       :host {
         max-width: 500px;
       }
+      @media print {
+        tr {
+          display: flex;
+        }
+        th,
+        td {
+          flex: 1;
+        }
+      }
     }
   </style>
 `;
