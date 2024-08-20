4b1769ca76cef308c65ec8d11b940fb4a993ef2b
Dan <dan.cioara@nordlogic.com>
2024-08-20 19:23:38 +0300

    PRP migration

--- elements/disaggregations/disaggregation-table-cell.ts
+++ elements/disaggregations/disaggregation-table-cell.ts
@@ -42,6 +42,7 @@ class DisaggregationTableCell extends UtilsMixin(LitElement) {
     }
     if (changedProperties.has('data')) {
       this.noValue = this._computeNoValue(this.data);
+      this.requestUpdate();
     }
   }
 
