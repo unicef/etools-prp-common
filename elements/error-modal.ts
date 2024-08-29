d8e5fec458ce3bd83aaa7ee821c2a639bfbc8821
Dan <dan.cioara@nordlogic.com>
2024-08-29 14:50:55 +0300

    PRP migration error modal fixes

--- elements/error-modal.ts
+++ elements/error-modal.ts
@@ -16,8 +16,11 @@ export class ErrorModal extends LitElement {
         etools-dialog {
           --divider-color: transparent;
         }
+        etools-dialog::part(header) {
+          display: none;
+        }
       </style>
-      <etools-dialog dialog-title="${translate('ERROR')}" hide-confirm-btn .cancelBtnText="${translate('CLOSE')}">
+      <etools-dialog hide-confirm-btn .cancelBtnText="${translate('CLOSE')}">
         <div>
           <ul>
             ${(this.localizedErrors || []).map((localizedError: any) => html`<li>${localizedError}</li>`)}
@@ -55,16 +58,16 @@ export class ErrorModal extends LitElement {
     const localizedErrors = errors.map(function (error) {
       switch (error) {
         case 'You have not selected overall status for one of Outputs':
-          return getTranslation('not_selected_overall_status');
+          return getTranslation('NOT_SELECTED_OVERALL_STATUS');
         case 'You have not completed Partner Contribution To Date field on Other Info tab.':
-          return getTranslation('not_completed_partner_contribution');
+          return getTranslation('NOT_COMPLETED_PARTNER_CONTRIBUTION');
         case 'You have not completed Challenges / bottlenecks in the reporting period field on Other Info tab.':
-          return getTranslation('not_completed_challenges_bottlenecks');
+          return getTranslation('NOT_COMPLETED_CHALLENGES_BOTTLENECKS');
         case 'You have not completed Proposed way forward field on Other Info tab.':
-          return getTranslation('not_completed_proposed_way');
+          return getTranslation('NOT_COMPLETED_PROPOSED_WAY');
         case 'You have not completed all indicator location data across all indicator reports for this progress' +
           ' report.':
-          return getTranslation('not_completed_indicator_location');
+          return getTranslation('NOT_COMPLETED_INDICATOR_LOCATION');
         default:
           return error;
       }
